dac960d05aa32cb19100a7aa4f71f18b0dc366b1
HP <HP@LAPTOP-REUQ2DKO>
2024-06-13 13:25:11 +0530

    second program

---
+++ First Project/src/com/test/ajdbakjbd.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class ajdbakjbd {
+	
+		public static void main(String[] args) {
+			
+			System.out.println("======================== Hello World ====================");
+		
+		}
+		
+	}
+
+
